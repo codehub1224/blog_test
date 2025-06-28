2dda310d46cc6340abcd1393a967d93f0c8a1941
codehub1224 <codehub.apps@gmail.com>
2025-06-28 23:41:49 +0530

    Add files via upload

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="min-h-screen bg-gradient-to-br from-secondary-50 to-primary-50">
+    <TheHeader />
+    <main class="pt-20">
+      <NuxtPage />
+    </main>
+    <TheFooter />
+  </div>
+</template>
+
+<script setup>
+useHead({
+  htmlAttrs: {
+    lang: 'en'
+  }
+})
+</script>
